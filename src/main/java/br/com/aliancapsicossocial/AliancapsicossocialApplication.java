c02cb6e26646445086b8e7b97034e2416170ce7b
Guilherme Momolli <guilherme.momolli@gmail.com>
2026-06-02 21:08:23 -0300

    feat: initialize Spring Boot project with Maven wrapper and essential dependencies

---
+++ src/main/java/br/com/aliancapsicossocial/AliancapsicossocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.aliancapsicossocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliancapsicossocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliancapsicossocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aliancapsicossocial/AliancapsicossocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aliancapsicossocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliancapsicossocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
